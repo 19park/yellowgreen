1c78f51b476e230b3759912482951c2f96506f4a
19park <19park@wholedoc.net>
2018-08-16 18:12:58 +0900

    image

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,44 @@
 <template>
-  <section class="container">
+  <section class="container" :style="{ backgroundColor: `yellowgreen`}">
+	<!--<img src="~/assets/y1.png"/>//-->
     <div>
-      <app-logo/>
       <h1 class="title">
         yellowgreen
       </h1>
       <h2 class="subtitle">
         yellowgreen project
       </h2>
+		<div class="cell-container">
+			<div class="cell" 
+			 :style="{ 
+			 backgroundImage: `url(${backgroundUrl})` ,
+			 backgroundSize: 'cover' 
+			 }"
+			 @click="anim">
+			 </div>
+		</div>
     </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import backgroundUrl from '~/assets/y1.png'
+
 
 export default {
+	data (){
+		return{
+			backgroundUrl
+		}
+	},
   components: {
     AppLogo
+  },
+  methods: {
+	anim() {
+		
+	}
   }
 }
 </script>
@@ -50,6 +71,33 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.cell-container {
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.cell-container .cell {
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: 1px solid #000;
+}
+
+.cell-container .cell:nth-child(3n) {
+  margin-right: 0;
+}
+.cell-container .cell:nth-child(27n) {
+  margin-bottom: 0;
+}
+
+.cell-move {
+  transition: transform 1s;
 }
 </style>
 
